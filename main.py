2c14ec62e2cec9f55261ee5d689958815f31ea79
Michael <8mleung8@gmail.com>
2023-11-01 03:06:13 -0400

    Finished Sudoku Solver
    NEXT:
    GUI

--- main.py
+++ main.py
@@ -10,7 +10,50 @@ board = [
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
 
-def createBoard(board):
+def findEmpty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return i, j
+    return False
+
+def checkValidNum(board,position,number):
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and i != position[1]:
+            return False
+
+    for i in range(len(board)):
+        if(board[i][position[1]] == number and i != position[0]):
+            return False
+
+    columnX = (position[1] // 3) * 3
+    columnY = (position[0] // 3) * 3
+
+    for i in range(columnY, columnY + 3):
+        for j in range(columnX, columnX + 3):
+            if board[i][j] == number and (i, j) != position:
+                return False
+
+    return True
+
+def solveSudoku(board):
+    if not findEmpty(board):
+        return True
+    else:
+        row, col = findEmpty(board)
+
+    for i in range(1,10):
+        if(checkValidNum(board,(row,col),i)):
+            board[row][col] = i
+
+            if(solveSudoku(board)):
+                return True
+
+            board[row][col] = 0
+    return False
+
+def displaySolution(board):
+    solveSudoku(board)
     for i in range(0,len(board)):
         if(i % 3 == 0 and i != 0):
             print("-------------------------")
@@ -20,9 +63,4 @@ def createBoard(board):
             print(str(board[i][j]) + " ",end = "")
         print("")
 
-def findEmpty(board):
-    for i in range(len(board)):
-        for j in range(len(board[0])):
-            if board[i][j] == 0:
-                return i, j
-createBoard(board)
+displaySolution(board)
