f9d74a771cd39d1e61388058864ae8d705404ee0
Michael <8mleung8@gmail.com>
2024-07-24 23:08:09 -0400

    Serve React frontend with Flask backend

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../client/build", static_url_path="/")
 CORS(app)
 
 def findEmpty(board):
@@ -68,6 +69,14 @@ def solve():
         return jsonify({"steps": steps, "errorList": errorList})
     else:
         return jsonify({"error": "Board cannot be solved"}), 400
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
     app.run()
